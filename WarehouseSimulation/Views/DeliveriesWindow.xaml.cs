14f9a6d04ff104951a63d107eeff01159b4c167a
dkrishen <d.krishen@softteco.com>
2023-07-24 14:46:33 +0300

    added base UI

---
+++ WarehouseSimulation/Views/DeliveriesWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WarehouseSimulation.Views
+{
+    /// <summary>
+    /// Interaction logic for DeliveriesPage.xaml
+    /// </summary>
+    public partial class DeliveriesWindow : Window
+    {
+        public DeliveriesWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WarehouseSimulation/Views/ProductWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using WarehouseSimulation.ViewModels;
+
+namespace WarehouseSimulation.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class ProductWindow : Window
+    {
+        public ProductWindow()
+        {
+            InitializeComponent();
+            DataContext = new ProductVM();
+        }
+    }
+}
