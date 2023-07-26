30b452890e84f34d9c39bbceb96b883d39f32487
dkrishen <d.krishen@softteco.com>
2023-07-26 16:22:59 +0300

    maked base UI for all pages

---
+++ WarehouseSimulation/Views/WarehouseView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using WarehouseSimulation.ViewModels;
+
+namespace WarehouseSimulation.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class WarehouseView : UserControl
+    {
+        public WarehouseView()
+        {
+            InitializeComponent();
+        }
+    }
+}
